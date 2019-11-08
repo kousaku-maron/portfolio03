e6f2f11523effe845a32bbb262d89facac78de51
kosaku kurino <kousaku.maron@gmail.com>
2019-11-08 15:04:32 +0900

    feat: firebase init and create hosting env

---
+++ hosting/webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    port: 3000,
+    hot: true,
+    open: true,
+    historyApiFallback: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js(x?)$/,
+        loader:'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.ts(x?)$/,
+        loader:'ts-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(png|jpg|gif)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ],
+  resolve: {
+    extensions: ['.js','.ts','.tsx']
+  }
+}
